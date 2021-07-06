9271e2d7001338b574491a0ba94c19c5e19f3304
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-07-06 12:40:31 +0300

    The user can now order the game panels as it wishes

--- addNewGameScript.js
+++ addNewGameScript.js
@@ -1,13 +1,36 @@
 const addNewGameButton = document.getElementById('newGame')
 
 let panelCounter = 0
+let dropZoneContainer = document.getElementById('dropZoneContainer')
 addNewGameButton.addEventListener('click',(event)=>
-{
-    let newElem = document.createElement('DIV')
-    newElem.className='individualGamePanel'
-    newElem.id=`${panelCounter}`
-    newElem.draggable=true
-    document.getElementById('gamesPanelContainer').appendChild(newElem)
+{   
+    if(panelCounter>=4)
+    {
+        alert('ERROR! YOU CANNOT ADD MORE THAN THE CURRENT GAME PANELS')
+    }
+    else
+    {
+        
+        if(panelCounter==0)
+        {
+            let newElem = document.createElement('DIV')
+            newElem.className='individualGamePanel'
+            newElem.id=`${panelCounter}`
+            newElem.innerText=`${panelCounter}`
+            
+            newElem.draggable=true
+            document.getElementById('dropZoneContainer').appendChild(newElem)
+        }
+        else
+        {
+            let newElem = document.createElement('DIV')
+            newElem.className='individualGamePanel'
+            newElem.id=`${panelCounter}`
+            newElem.innerText=`${panelCounter}`
+            newElem.draggable=true
+            document.getElementById('gamesPanelContainer').appendChild(newElem)
+        }    
+    }
     panelCounter+=1
 
 })
--- drag-drop.js
+++ drag-drop.js
@@ -2,16 +2,23 @@ let draggedElement = ''
 document.addEventListener("drag",(event) => {
 });
 document.addEventListener('dragstart', (event)=>{
+    console.log('dragging started')
     draggedElement = event.target
 })
 document.addEventListener('dragend', (event)=>{
+    console.log('dragging ended')
+
 })
 
 
 document.addEventListener('dragover', (event)=>{
+    console.log('dragging over')
+
     event.preventDefault()
 })
 document.addEventListener('dragenter', (event)=>{
+    console.log('dragging entered')
+
     if(event.target.className=='dropzone')
     {
         event.target.style='opacity:0.5'
@@ -19,6 +26,8 @@ document.addEventListener('dragenter', (event)=>{
     }
 },false)
 document.addEventListener('dragleave', (event)=>{
+    console.log('dragging left')
+
     if(event.target.className=='dropzone')
     {
         event.target.style=''
@@ -27,6 +36,8 @@ document.addEventListener('dragleave', (event)=>{
 },false)
 
 document.addEventListener('drop', (event)=>{
+    console.log('dragging dropped')
+
     if(event.target.className==='dropzone')
     {
         event.target.appendChild(draggedElement)
