abe239fb807a3dc434ebdce80c43324741600f80
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-07-06 21:36:41 +0300

    GamePanels are now added left to right, up down across the entire admin panel

--- addNewGameScript.js
+++ addNewGameScript.js
@@ -1,44 +1,83 @@
 const addNewGameButton = document.getElementById('newGame')
+let adminPanelContainer = document.getElementById('adminPanelContainer')
+let maxWidth = adminPanelContainer.clientWidth
+let maxHeight = adminPanelContainer.clientHeight
 
-let panelCounter = 0
-let dropZoneContainer = document.getElementById('dropZoneContainer')
-let panels = []
-addNewGameButton.addEventListener('click',(event)=>
-{   
-    if(scoreboards.length>1)
+let newWidgetObj;
+let gamePanelWidgets=[]
+let finalWidgetsToBeShown=[]
+let objectCounter = 0
+let isValidX = false
+let isValidY = false
+let x = 0
+let y = 0
+class GamePanel
+{
+    constructor(width, height, x, y)
     {
-        alert('NOT ENOUGH SPACE ON THE ADMIN PANEL TO ADD GAME PANELS!')
+        this.width=width,
+        this.height = height,
+        this.x=x,
+        this.y=y
     }
+}
+
+addNewGameButton.addEventListener('click',(event)=>{
+    // create initial widget object with absolute X and Y coords if there are no elements in the list, prior to this one
+    if(objectCounter<=0)
+    {
+        newWidgetObj = new GamePanel(300,100,x,y)
+        gamePanelWidgets.push(newWidgetObj)
+
+    }
+    // - get the previous object in the list and when creating the new object add the width and height to the last object's X and Y if they are, of course, legal variables
     else
     {
-        if(panelCounter>=4)
+        x+=300
+        y+=300
+        if(x>maxWidth)
         {
-            alert('ERROR! YOU CANNOT ADD MORE THAN THE CURRENT GAME PANELS')
+            x=maxWidth
+            
         }
         else
         {
-            
-            if(panelCounter==0)
-            {
-                let newElem = document.createElement('DIV')
-                newElem.className='individualGamePanel'
-                newElem.id=`${panelCounter}`
-                newElem.innerText=`${panelCounter}`
-                
-                newElem.draggable=true
-                document.getElementById('dropZoneContainer').appendChild(newElem)
-            }
-            else
-            {
-                let newElem = document.createElement('DIV')
-                newElem.className='individualGamePanel'
-                newElem.id=`${panelCounter}`
-                newElem.innerText=`${panelCounter}`
-                newElem.draggable=true
-                document.getElementById('gamesPanelContainer').appendChild(newElem)
-            }    
+            isValidX=true
         }
+        if(y>maxHeight)
+        {
+            y=maxHeight
+        }
+        else
+        {
+            isValidY=true
+        }
+        if(isValidX && isValidY)
+        {
+            newWidgetObj = new GamePanel(300,100,x,y)
+            // save the objects in a list  
+            gamePanelWidgets.push(newWidgetObj)
+        }
+
     }
-    panels.push(1)
-    panelCounter+=1
+    objectCounter+=1
+    // create a new HTML element by the objects in the list
+    createElements()
+
 })
+
+function createElements()
+{
+    let gamePanelContainer = document.getElementById('gamesPanelContainer')
+    for(element of gamePanelWidgets)
+    {
+        console.log(element)
+        const newElement = document.createElement('div');
+        newElement.draggable=true
+        newElement.classList.add('individualGamePanel');
+        const newElementGrid = document.querySelector('.gamesPanel');
+        newElementGrid.appendChild(newElement);
+
+    }
+    gamePanelWidgets=[]
+}
--- drag-drop.js
+++ drag-drop.js
@@ -3,33 +3,39 @@ document.addEventListener("drag",(event) => {
 });
 document.addEventListener('dragstart', (event)=>{
     draggedElement = event.target
+
 })
 document.addEventListener('dragend', (event)=>{
 })
 document.addEventListener('dragover', (event)=>{
 
     event.preventDefault()
+    if(event.pageX>maxWidth)
+    {
+        console.log('Invalid X')
+        event.pageX=0
+    }
+    else if(event.pageY>maxHeight)
+    {
+        console.log('Invalid Y')
+        event.pageY=0
+    }
+    console.log(event.pageX, event.pageY)
 })
 document.addEventListener('dragenter', (event)=>{
-    if(event.target.className=='dropzone')
+    console.log(event.target.className)
+    if(event.target.className=='gamesPanelContainer')
     {
-        event.target.style='opacity:0.5'
-        event.target.style='background-color:gray'
+
     }
 },false)
 document.addEventListener('dragleave', (event)=>{
-    if(event.target.className=='dropzone')
+    if(event.target.className=='gamesPanelContainer')
     {
-        event.target.style=''
-        event.target.style=''
+
      }
 },false)
 
 document.addEventListener('drop', (event)=>{
-    if(event.target.className==='dropzone')
-    {
-        event.target.appendChild(draggedElement)
-        event.target.style=''
-        event.target.style=''
-    }
+    
 })
