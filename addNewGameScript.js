3044d7cebbf92f93e3c5f81ddfa662be43ca4c53
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-07-06 10:53:46 +0300

    Fixed the bug where dragging one panel would drag all panels as one instead. I also added incrementing IDs to the panels that are being created upon pressing the respective button

--- addNewGameScript.js
+++ addNewGameScript.js
@@ -1,9 +1,13 @@
 const addNewGameButton = document.getElementById('newGame')
 
+let panelCounter = 0
 addNewGameButton.addEventListener('click',(event)=>
 {
-    //create div element here
     let newElem = document.createElement('DIV')
     newElem.className='individualGamePanel'
+    newElem.id=`${panelCounter}`
+    newElem.draggable=true
     document.getElementById('gamesPanelContainer').appendChild(newElem)
+    panelCounter+=1
+
 })
