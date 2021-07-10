77b1a6a6d6c7dc7a902d612db6c22273da7d301a
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-07-10 20:01:30 +0300

    Improved the admin panel width increasement algorithm. Instead of listening to a mousedown event, the algorithm is now listening for a mousemove event. That way the adminPanel will enlarge itself even if the user does not want to add any more widgets. Before the widget wouldve stood on the border of the adminpanbbel waiting for the next widget to be added so that the adminpanel can elarge itself.

--- overflowDetector.js
+++ overflowDetector.js
@@ -1,7 +1,7 @@
 let elementContainer = document.querySelector('#adminPanelContainer')
-document.addEventListener('mousedown',()=>{
-    if (elementContainer.offsetHeight < elementContainer.scrollHeight ||
-    elementContainer.offsetWidth < elementContainer.scrollWidth)
+document.addEventListener('mousemove',()=>{
+    if (elementContainer.offsetHeight <= elementContainer.scrollHeight ||
+    elementContainer.offsetWidth <= elementContainer.scrollWidth)
     {
         elementContainer.style.height=elementContainer.clientHeight+300
         maxHeight=elementContainer.clientHeight+300
