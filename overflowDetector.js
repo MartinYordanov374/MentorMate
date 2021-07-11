eb9cabcbf558cb53d7f04c85964c6d6868d6c498
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-07-11 10:40:03 +0300

    Fixed the bugs in the overflow detection script, where a overflow would be register whenever the widgets are on the border of the admin panel

--- overflowDetector.js
+++ overflowDetector.js
@@ -1,7 +1,7 @@
 let elementContainer = document.querySelector('#adminPanelContainer')
 document.addEventListener('mousemove',()=>{
-    if (elementContainer.offsetHeight <= elementContainer.scrollHeight ||
-    elementContainer.offsetWidth <= elementContainer.scrollWidth)
+    if (elementContainer.offsetHeight < elementContainer.scrollHeight ||
+    elementContainer.offsetWidth < elementContainer.scrollWidth)
     {
         elementContainer.style.height=elementContainer.clientHeight+300
         maxHeight=elementContainer.clientHeight+300
