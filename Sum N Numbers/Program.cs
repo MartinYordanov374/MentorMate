b7aa9a344e08bb66d878cb2e4d58e9f2eaa18c26
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-07 13:57:32 +0300

    Added the Sum N Numbers problems

---
+++ Sum N Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+ 
+namespace Sum_N_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // number input, integer, bigger than 1
+            int n = int.Parse(Console.ReadLine());
+            // sum the first numbers from 1 to n
+            int sum = (n*((n+1)))/2;
+            Console.WriteLine(sum);
+        }
+    }
+}
