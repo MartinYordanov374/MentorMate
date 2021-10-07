ec65c8e8cd4264c17de3fd5447dc76a8b1965530
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-07 14:03:55 +0300

    Added the reverse numbers problem

---
+++ Reverse Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ 
+namespace Reverse_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // enter number and reverse it
+            int numberInput = int.Parse(Console.ReadLine());
+            string numberReversed = "";            
+ 
+            string numberToString = numberInput.ToString();
+            for(int i = numberToString.Length - 1; i >= 0; i--)
+            {
+                numberReversed += numberToString[i];
+            }
+            Console.WriteLine(numberReversed);
+        }
+        
+    }
+}
