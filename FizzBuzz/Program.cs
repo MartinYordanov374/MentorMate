1a454a77524ad48389a30979a74ebc4313aaa10c
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-07 13:54:56 +0300

    Added the FizzBuzz problem

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if(i % 15 == 0)
+                {
+                   Console.WriteLine(i + " - FizzBuzz");
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine(i + " - Buzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    System.Console.WriteLine(i + " - Fizz");
+                }
+            }
+        }
+
+    }
+}
