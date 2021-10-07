9f9ca2a50865cd8932b95550d37c8193c235d993
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-07 13:56:18 +0300

    Added the Reverse String problem

---
+++ Reverse String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+ 
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // enter number and reverse it
+            string input = Console.ReadLine();
+            string inputReversed = "";            
+ 
+            for(int i = input.Length - 1; i >= 0; i--)
+            {
+                inputReversed += input[i];
+            }
+            Console.WriteLine(inputReversed);
+        }
+        
+    }
+}
