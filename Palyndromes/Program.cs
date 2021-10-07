d20fa51ebccf70162a7a0458a067cf91ba15dda1
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-07 13:59:13 +0300

    Added the Palyndromes problem

---
+++ Palyndromes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+ 
+namespace Palyndromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // enter number and reverse it
+            string input = Console.ReadLine();
+            string inputReversed = "";            
+            // Regex whitespaceRegex = new Regex(@"\S+");
+            for(int i = input.Length - 1; i >= 0; i--)
+            {
+                inputReversed += input[i];
+            }
+            string resultInput = Regex.Replace(input,@"\s+","");
+            string resultInputReversed = Regex.Replace(inputReversed, @"\s+", "");
+            if(resultInput.ToLower() == resultInputReversed.ToLower())
+            {
+                Console.WriteLine($"{input} is a palyndrome.");
+            }
+            else
+            {
+               Console.WriteLine($"{input} is not a palyndrome.");
+            }
+ 
+        }
+    }
+}
