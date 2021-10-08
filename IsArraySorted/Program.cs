0096d66ca0582d639d2b9a96a0123a675c756d43
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-08 14:24:19 +0300

    Made the code more readable

--- IsArraySorted/Program.cs
+++ IsArraySorted/Program.cs
@@ -10,24 +10,28 @@ namespace IsArraySorted
             bool isSorted = false;
             for (int i = 0; i < numbersArray.Length-1; i++)
             {
+
                 if(numbersArray[i]>numbersArray[i+1])
                 {
                     isSorted = true;
                 }
+
                 else
                 {
                     isSorted = false;
                 }
+
             }
+
             if(isSorted)
             {
                 System.Console.WriteLine("Your array is sorted");
             }
+
             else
             {
                 System.Console.WriteLine("Your array is not sorted");
             }
-
 
         }
 
