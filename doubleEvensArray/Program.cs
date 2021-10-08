b745f8a8f3d179f94ee09d5328d6c98576878925
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-08 11:15:49 +0300

    Added the doubleEvensArray

---
+++ doubleEvensArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace doubleEvensArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your array size");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] numbersArray = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter the element at index {i}: ");
+                numbersArray[i] = double.Parse(Console.ReadLine());
+                if(numbersArray[i] % 2 == 0)
+                {
+                    numbersArray[i] = numbersArray[i] * 2;
+                }
+            }
+
+            foreach (double item in numbersArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
