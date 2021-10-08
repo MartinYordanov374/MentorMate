fc6cd873b8d93bde19a95939d6428b014277d51e
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-08 12:45:20 +0300

    Added the array sorted problem

---
+++ IsArraySorted/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IsArraySorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbersArray = new double[]{56,23,0,1};
+            bool isSorted = false;
+            for (int i = 0; i < numbersArray.Length-1; i++)
+            {
+                if(numbersArray[i]>numbersArray[i+1])
+                {
+                    isSorted = true;
+                }
+                else
+                {
+                    isSorted = false;
+                }
+            }
+            if(isSorted)
+            {
+                System.Console.WriteLine("Your array is sorted");
+            }
+            else
+            {
+                System.Console.WriteLine("Your array is not sorted");
+            }
+
+
+        }
+
+    }
+}
