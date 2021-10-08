d9f4a940ad80eaaea3b30959a86c22e991b0debb
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-08 14:23:14 +0300

    Made the code more readdable

--- MaxSequenceArray/Program.cs
+++ MaxSequenceArray/Program.cs
@@ -10,29 +10,38 @@ namespace MaxSequenceArray
             double[] array = new double[]{2, 1, 1, 2, 3, 3, 2, 2, 2, 1};
 
             List<double> outerSequenceKeeper = new List<double>();
+
             for (int i = 0; i < array.Length; i++)
             {
                 List<double> innerSequenceKeeper = new List<double>();
+
                 for (int j = i; j < array.Length; j++)
                 {
+
                     if(array[i] == array[j])
                     {
                         innerSequenceKeeper.Add(array[i]);
                     }
+
                     else
                     {
                         break;
                     }
+                    
                 }
+
                 if(innerSequenceKeeper.Count > outerSequenceKeeper.Count)
                 {
                     outerSequenceKeeper = innerSequenceKeeper;
                 }
+
             }
-            foreach (var item in outerSequenceKeeper)
+
+            foreach (double item in outerSequenceKeeper)
             {
                 Console.WriteLine(item);
             }
+            
         }
     }
 }
