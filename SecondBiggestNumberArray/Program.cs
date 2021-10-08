26572d2e9635d7ba33ee3f7529a211938f2ba2c2
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-08 14:31:32 +0300

    made the code more readable

--- SecondBiggestNumberArray/Program.cs
+++ SecondBiggestNumberArray/Program.cs
@@ -7,14 +7,17 @@ namespace SecondBiggestNumberArray
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine($"Enter array size: ");
+            Console.WriteLine($"Enter array size: ");
             int n = int.Parse(Console.ReadLine());
+
             int[] numberArray = new int[n];
+
             for (int i = 0; i < n; i++)
             {
-                System.Console.WriteLine($"Enter element at index {i}: ");
+                Console.WriteLine($"Enter element at index {i}: ");
                 numberArray[i] = int.Parse(Console.ReadLine());
             }
+            
             Console.WriteLine($"The second biggest number in your array is {MergeSort(numberArray)}.");
         }
 
@@ -32,40 +35,49 @@ namespace SecondBiggestNumberArray
 
             for (int i = midPoint-1; i < unsortedArray.Length; i++)
             {
-                // System.Console.WriteLine(i);
                 rightSide.Add(unsortedArray[i]);
             }
             
             leftSide = SortList(leftSide);
+
             rightSide = SortList(rightSide);
+
             List<int> sortedMergedList = SortList(MergeList(leftSide,rightSide)).Distinct().ToList();
 
             int secondBiggestNumber = sortedMergedList[sortedMergedList.Count-2];
+            
             return secondBiggestNumber;
 
         }
 
         static List<int> SortList(List<int> targetList)
         {
+
             for (int i = 0; i < targetList.Count-1; i++)
             {
+
                 for (int j = 0; j < targetList.Count- i - 1; j++)
                 {
+                    
                     if(targetList[j]>targetList[j+1])
                     {
                         int temp = targetList[j]; 
                         targetList[j] = targetList[j+1]; 
                         targetList[j+1] = temp; 
                     }
+
                 }
+
             }
+
             return targetList;
         }
 
         static List<int> MergeList(List<int> firstList, List<int> secondList)
         {
+
             List<int> mergedLists = new List<int>();
-            return mergedLists.Concat(firstList).Concat(secondList).ToList();
+            return mergedLists = mergedLists.Concat(firstList).Concat(secondList).ToList();
         }
     }
 }
