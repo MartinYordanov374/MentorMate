c04c78a64e1a36c7ac386e63f504d557a0415a5d
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-14 13:36:37 +0300

    Made tghecode more readable

--- SeaChess/Program.cs
+++ SeaChess/Program.cs
@@ -14,93 +14,134 @@ namespace SeaChess
         static void Main(string[] args)
         {
             char[,] gameBoard = initBoard();
-            printBoard(gameBoard);
-
             int xCoord = 0;
             int yCoord = 0;
-            int playerCounter = 0; // even numbers for player 1 and odd numbers for player 2
+            int playerCounter = 0; 
             int currentPlayer = 1;
-            for (int i = 0; i < gameBoard.GetLength(0); i++)
-            {
-                for (int j = 0; j < gameBoard.GetLength(1); j++)
-                {
-                    
-                    currentPlayer = playerCounter % 2 == 0 ? currentPlayer = 1 : currentPlayer = 2; 
+            char currentPlayerMark = 'X';
+
+            for (int i = 0; i < MAX_NUMBER_OF_MOVES; i++)
+            {                    
+                    currentPlayer = playerCounter % 2 == 0 ? 1 : 2; 
+
+                    printBoard(gameBoard);
+                    if(CheckForWinningMove(gameBoard,currentPlayerMark))
+                    {
+                        printBoard(gameBoard);
+                        Console.WriteLine($"Player {currentPlayerMark} wins");
+                        break;
+                    }
                     if(currentPlayer == 1)
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        currentPlayerMark = 'X';
+
                         Console.WriteLine("Player 1's turn. -> Place an X");
                     }
                     else
                     {
-                        System.Console.WriteLine("Player 2's turn -> Place an O");
-                    }
-                   Console.WriteLine("Enter x coordinate: ");
-                   xCoord = promptUserForX(int.Parse(Console.ReadLine()));
-                                   
-                   Console.WriteLine("Enter y coordinate: ");
-                   yCoord = promptUserForY(int.Parse(Console.ReadLine()));
+                        Console.ForegroundColor = ConsoleColor.Green;
 
-                   List<dynamic> isTakenList = checkIfGameFieldTaken(xCoord, yCoord, gameBoard);
+                        currentPlayerMark = 'O';
+
+                        Console.WriteLine("Player 2's turn -> Place a O");
+                    }
+            
+                   Console.WriteLine("Enter x coordinate [0-2]: ");
+                   xCoord = promptUserForX(int.Parse(Console.ReadLine()), gameBoard, currentPlayerMark);
+                                   
+                   Console.WriteLine("Enter y coordinate [0-2]: ");
+                   yCoord = promptUserForY(int.Parse(Console.ReadLine()), gameBoard, currentPlayerMark);
+
+                   List<dynamic> isTakenList = checkIfGameFieldTaken(xCoord, yCoord, gameBoard,currentPlayerMark);
 
                    if(isTakenList[0] == false)
                    {
                         if(currentPlayer == 1)
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
                             gameBoard[isTakenList[1],isTakenList[2]] = 'X';
                         }
                         else
                         {
-                            Console.ForegroundColor = ConsoleColor.Green;
-
                             gameBoard[isTakenList[1],isTakenList[2]] = 'O';
                         }
-                        if(checkIfWinningHorizontally(gameBoard) == true || checkIfWinningVertically(gameBoard) == true || checkIfWinningDiagonally(gameBoard) == true)
-                        {
-                            System.Console.WriteLine("WIN");
-                            printBoard(gameBoard);
-                            break;
-                        }
                    }
-                    printBoard(gameBoard);
-
                    playerCounter++;
+            }
+        }
+        static bool CheckForWinningMove(char[,] board, char playerMark)
+        {
+            int playerMarkCounter = 0;
 
+            for (int i = 0; i < BOARD_ROWS; i++)
+            {
+                for (int j = 0; j < BOARD_COLUMNS; j++)
+                {
+                    if(board[i, j] == playerMark)
+                    {
+                        playerMarkCounter++;
+                    }
                 }
+
+                if(playerMarkCounter == BOARD_ROWS)
+                {
+                    return true;
+                }
+
+                playerMarkCounter = 0;
             }
 
-        }
-        static int promptUserForX(int xCoordinate)
-        {
-            
-            if(xCoordinate > 2 || xCoordinate < 0)
+            for (int i = 0; i < BOARD_COLUMNS; i++)
             {
-                Console.WriteLine("Please enter a valid x coordinate [0-2]: ");
+                for (int j = 0; j < BOARD_ROWS; j++)
+                {
+                    if (board[i, j] == playerMark)
+                    {
+                        playerMarkCounter++;
+                    }
+                }
 
-                xCoordinate = int.Parse(Console.ReadLine());
+                if (playerMarkCounter == BOARD_COLUMNS)
+                {
+                    return true;
+                }
 
-                return promptUserForX(xCoordinate);
+                playerMarkCounter = 0;
             }
 
-            return xCoordinate;
-        }
-
-         static int promptUserForY(int yCoordinate)
-        {
-
-            if(yCoordinate > 2 || yCoordinate < 0)
+            if (BOARD_ROWS == BOARD_COLUMNS)
             {
-                Console.WriteLine("Please enter a valid y coordinate [0-2]: ");
+                for (int i = 0; i < BOARD_ROWS; i++)
+                {
+                    if(board[i, i] == playerMark) // checking horizontally
+                    {
+                        playerMarkCounter++;
+                    }
 
-                yCoordinate = int.Parse(Console.ReadLine());
+                    if (playerMarkCounter == BOARD_COLUMNS) 
+                    {
+                        return true;
+                    }
+                    
+                    playerMarkCounter = 0;
 
-                return promptUserForY(yCoordinate);
+                    if (board[(BOARD_ROWS -1) - i, i] == playerMark)
+                    {
+                        playerMarkCounter++;
+                    }
+
+                    if (playerMarkCounter == BOARD_COLUMNS)
+                    {
+                        return true;
+                    }
+                }
+
             }
-
-            return yCoordinate;
+            return false;
         }
 
-        static List<dynamic> checkIfGameFieldTaken(int xCoordinate, int yCoordinate, char[,] gameBoardState)
+        static List<dynamic> checkIfGameFieldTaken(int xCoordinate, int yCoordinate, char[,] gameBoardState, char currentPlayerMark)
         {            
             bool isTaken = false;
 
@@ -112,12 +153,12 @@ namespace SeaChess
                 Console.WriteLine("That field is already taken !");
                         
                 Console.WriteLine("Please enter a new x coordinate: ");
-                xCoordinate = promptUserForX(int.Parse(Console.ReadLine()));
+                xCoordinate = promptUserForX(int.Parse(Console.ReadLine()), gameBoardState,currentPlayerMark);
 
                 Console.WriteLine("Please enter a new y coordinate: ");
-                yCoordinate = promptUserForY(int.Parse(Console.ReadLine()));
+                yCoordinate = promptUserForY(int.Parse(Console.ReadLine()), gameBoardState, currentPlayerMark);
 
-                return checkIfGameFieldTaken(xCoordinate, yCoordinate, gameBoardState);
+                return checkIfGameFieldTaken(xCoordinate, yCoordinate, gameBoardState, currentPlayerMark);
             }
             coordinatesList.Add(isTaken);
             coordinatesList.Add(xCoordinate);
@@ -125,93 +166,45 @@ namespace SeaChess
 
             return coordinatesList;
         }
-
-        static bool checkIfWinningHorizontally(char[,] gameBoardState)
+        static int promptUserForX(int xCoordinate, char[,] gameBoard, char currentPlayerMark)
         {
-            
-            // check horizontally first
-            string rowOne = "";
-            string rowTwo = "";
-            string rowThree = "";
-            for (int i = 0; i < 3; i++)
+            if(xCoordinate > 2 || xCoordinate < 0)
             {
-                rowOne += gameBoardState[0,i];
+                Console.WriteLine("Please enter a valid x coordinate [0-2]: ");
+
+                xCoordinate = int.Parse(Console.ReadLine());
+
+                return promptUserForX(xCoordinate,gameBoard,currentPlayerMark);
             }
-            for (int i = 0; i < 3; i++)
-            {
-                rowTwo += gameBoardState[1,i];
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                rowThree += gameBoardState[2,i];
-            }
-            if((rowOne == "XXX" || rowOne == "OOO") || 
-                (rowTwo == "XXX" || rowTwo == "OOO") || 
-                (rowThree == "XXX" || rowThree == "OOO"))
-            {
-                return true;
-            }
-            return false;
+
+            return xCoordinate;
         }
 
-        static bool checkIfWinningVertically(char[,] gameBoardState)
+         static int promptUserForY(int yCoordinate, char[,] gameBoard, char currentPlayerMark)
         {
-            
-            // check horizontally first
-            string colOne = "";
-            string colTwo = "";
-            string colThree = "";
-            for (int i = 0; i < 3; i++)
+            if(CheckForWinningMove(gameBoard,currentPlayerMark))
             {
-                colOne += gameBoardState[i,0];
+                Console.WriteLine("WIN");
+                printBoard(gameBoard);
+                Environment.Exit(0);
             }
-            for (int i = 0; i < 3; i++)
+            if(yCoordinate > 2 || yCoordinate < 0)
             {
-                colTwo += gameBoardState[i,1];
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                colThree += gameBoardState[i,2];
-            }
-            if((colOne == "XXX" || colOne == "OOO") || 
-                (colTwo == "XXX" || colTwo == "OOO") || 
-                (colThree == "XXX" || colThree == "OOO"))
-            {
-                return true;
-            }
-            // check vertically second
-            
-            // finally check diagonally
-            return false;
-        }
-        static bool checkIfWinningDiagonally(char[,] gameBoardState)
-        {
-            
-            // check horizontally first
-            string diagonalOne = "";
-            string diagonalTwo = "";
-            for (int i = 0; i < 3; i++)
-            {
-                if(gameBoardState[i,i] != '-')
-                {
-                    diagonalOne += gameBoardState[i,i];
-                }
-            }
-            System.Console.WriteLine(diagonalOne);
-            if((diagonalOne == "XXX" || diagonalOne == "OOO"))
-            {
-                return true;
-            }
-            
-            return false;
-        }
+                Console.WriteLine("Please enter a valid y coordinate [0-2]: ");
 
+                yCoordinate = int.Parse(Console.ReadLine());
+
+                return promptUserForY(yCoordinate, gameBoard, currentPlayerMark);
+            }
+
+            return yCoordinate;
+        }
         static void printBoard(char[,] gameBoard)
         {
             Console.Clear();
-            for (int row = 0; row < gameBoard.GetLength(0); row++)
+            for (int row = 0; row < BOARD_ROWS; row++)
             {
-                for (int col = 0; col < gameBoard.GetLength(1); col++)
+                for (int col = 0; col < BOARD_COLUMNS; col++)
                 {
                     
                     Console.Write(string.Format("{0} ", gameBoard[row, col]));
