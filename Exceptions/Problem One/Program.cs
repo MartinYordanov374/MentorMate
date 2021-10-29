9e4512288a3b5e48081464403a0b2d89c795f747
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-29 11:33:20 +0300

    Added the first exceptions problem

---
+++ Exceptions/Problem One/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Problem_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadNumber(1,10);
+        }
+        static void ReadNumber(int start, int end)
+        {
+            double number = double.Parse(Console.ReadLine());
+            if(number >= start && number <= end)
+            {
+                System.Console.WriteLine($"Your number is {number}");
+            }
+            else if(number <= 1 || number >= 100)
+            {
+                throw new Exception("Your number is invalid. Is it bigger or equal to 1 or 100?");
+            }
+            else
+            {
+                throw new Exception("Your number was not in the interval you have provided.");
+            }
+        }
+    }
+}
