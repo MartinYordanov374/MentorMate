aebfd1dbdc5d8c8abf5e54605c6b0ed6a480a121
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-29 12:30:12 +0300

    Updated the problem

--- Exceptions/Problem One/Program.cs
+++ Exceptions/Problem One/Program.cs
@@ -5,33 +5,52 @@ namespace Problem_One
     {
         static void Main(string[] args)
         {
-            ReadNumber(1,10);
+            ReadNumber(1,11);
         }
         static void ReadNumber(int start, int end)
         {
             try
             {
-                for(int i = 0; i < 10; i++)
-                {
-                    double number = double.Parse(Console.ReadLine());
-                    if(number >= start && number <= end)
+                    double numberOne = double.Parse(Console.ReadLine());
+                    double numberTwo = double.Parse(Console.ReadLine());
+                    double numberThree = double.Parse(Console.ReadLine());
+                    double numberFour = double.Parse(Console.ReadLine());
+                    double numberFive = double.Parse(Console.ReadLine());
+                    double numberSix = double.Parse(Console.ReadLine());
+                    double numberSeven = double.Parse(Console.ReadLine());
+                    double numberEight = double.Parse(Console.ReadLine());
+                    double numberNine = double.Parse(Console.ReadLine());
+                    double numberTen = double.Parse(Console.ReadLine());
+
+                    bool areValidNumbers = 1 < numberOne 
+                    && numberOne < numberTwo 
+                    && numberTwo < numberThree 
+                    && numberThree < numberFour 
+                    && numberFour < numberFive 
+                    && numberFive < numberSix 
+                    && numberSix < numberSeven 
+                    && numberSeven < numberEight  
+                    && numberEight < numberNine 
+                    && numberNine < numberTen 
+                    && numberTen < 100 ;
+
+                    System.Console.WriteLine(areValidNumbers);
+
+                    if( areValidNumbers )
                     {
-                        System.Console.WriteLine($"Your number is {number}");
+                        System.Console.WriteLine("Your numbers are valid");
                     }
-                    else if(number <= 1 || number >= 100)
+                    else if(areValidNumbers == false)
                     {
-                        throw new Exception("Your number is invalid. Is it bigger or equal to 1 or 100?");
+                        throw new Exception("Your number are invalid. Are they bigger or equal to 1 or 100 ?");
                     }
-                    else
-                    {
-                        throw new Exception("Your number was not in the interval you have provided.");
-                    }
-                }
+
+                
 
             }
             catch(Exception)
             {
-                System.Console.WriteLine("Invalid input");
+                System.Console.WriteLine("Invalid input. Your number is likely outside of the specified range or not a number. Consider that they could not be following the required sequence.");
             }
             
         }
