84a801061e4c67881bd2fbe7411f20dae5952af1
GoofyGalaxy374 <prophetyordanov@gmail.com>
2021-10-29 11:50:49 +0300

    Added the second problem

---
+++ Exceptions/Problem Two/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace Problem_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFilePath = @"./lorem.txt"; //This one works under Linux. The equivalent for this path would be @".\lorem.txt" under Windows
+            ReadFile(textFilePath);
+        }
+        static void ReadFile(string path)
+        {
+            try
+            {
+                string resultString = File.ReadAllText(path);
+                System.Console.WriteLine(resultString);
+            }
+            catch(FileNotFoundException) {
+                throw new Exception("It seems that the provided path is incorrect. Is your escaping correct?");
+            }
+        }
+    }
+}
